10b66ba5e9eb2d41233eb472de763ab73b47e8c4
lorenzopepe98 <lore.pepe998@gmail.com>
2020-04-28 21:30:21 +0200

    removed back button, as it goes against the browser flow and website pages will ba cached

--- Components/Blog/PostHeader.jsx
+++ Components/Blog/PostHeader.jsx
@@ -1,16 +1,20 @@
-import Link from "next/link";
-import { Back } from "../../assets/Back";
+// import Link from "next/link";
+// import { Back } from "../../assets/Back";
 
 export const PostHeader = ({ title, dateString }) => {
 	return (
 		<div className="post-header">
-			<Link href={`/blog`}>
-				<a>
-					<Back />
-				</a>
-			</Link>
 			<h1>{title}</h1>
 			<span className="date">{dateString}</span>
 		</div>
 	);
 };
+
+// don'think it's very important as it's more of a webapp functionality
+// pages will be cached anyway so it's not expensive to go back!
+
+// <Link href={`/blog`}>
+// <a>
+// 	<Back />
+// </a>
+// </Link>
