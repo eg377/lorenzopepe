5cbe12df75b886d351571ff0a883ba4cd56c88c0
lorenzopepe98 <lore.pepe998@gmail.com>
2020-04-28 17:00:21 +0200

    added one dark pro theme for codeblocks

---
+++ themes/OneDarkProTheme.js
@@ -0,0 +1,73 @@
+// @flow
+// Converted automatically using ./tools/themeFromVsCode
+
+let theme = {
+	plain: {
+		color: "#abb2bf",
+		backgroundColor: "#282c34",
+	},
+	styles: [
+		{
+			types: ["punctuation", "operator"],
+			style: {
+				color: "rgb(171, 178, 191)",
+			},
+		},
+		{
+			types: ["constant"],
+			style: {
+				color: "rgb(255, 255, 255)",
+			},
+		},
+		{
+			types: ["char"],
+			style: {
+				color: "rgb(97, 175, 239)",
+			},
+		},
+		{
+			types: ["keyword", "selector"],
+			style: {
+				color: "rgb(198, 120, 221)",
+			},
+		},
+		{
+			types: ["class-name", "attr-name", "string", "symbol"],
+			style: {
+				color: "rgb(86, 182, 194)",
+			},
+		},
+		{
+			types: ["function", "builtin", "namespace", "changed"],
+			style: {
+				color: "rgb(229, 192, 123)",
+			},
+		},
+		{
+			types: ["variable", "tag", "deleted"],
+			style: {
+				color: "rgb(224, 108, 117)",
+			},
+		},
+		{
+			types: ["number"],
+			style: {
+				color: "rgb(209, 154, 102)",
+			},
+		},
+		{
+			types: ["inserted"],
+			style: {
+				color: "rgb(152, 195, 121)",
+			},
+		},
+		{
+			types: ["comment"],
+			style: {
+				fontStyle: "italic",
+			},
+		},
+	],
+};
+
+export default theme;
