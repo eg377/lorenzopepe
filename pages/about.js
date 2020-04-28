6a4bb5388a9a4c5289fe10a1c7393178f9f490a5
lorenzopepe98 <lore.pepe998@gmail.com>
2020-04-29 01:05:44 +0200

    converted from mdx to js to support meta / seo

---
+++ pages/about.js
@@ -0,0 +1,24 @@
+import { Fragment } from "react";
+import Head from "next/head";
+import { WorkStatus } from "../Components/WorkStatus.jsx";
+import { Socials } from "../Components/Socials.jsx";
+import AboutMe from "../drafts/about-me.mdx";
+
+const About = () => {
+	return (
+		<Fragment>
+			<Head>
+				<meta name="description" content="About Me" />
+				<title>About Me</title>
+			</Head>
+			<div className="about-title">
+				<h2>Ciao! I'm Lorenzo 👋</h2>
+				<Socials />
+			</div>
+			<AboutMe />
+			<WorkStatus searching={true} />
+		</Fragment>
+	);
+};
+
+export default About;
