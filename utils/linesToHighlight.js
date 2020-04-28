6089cfd561617d20f9f529d67505fa6a48357ff7
lorenzopepe98 <lore.pepe998@gmail.com>
2020-04-28 16:59:34 +0200

    extract highlighted lines from codeblock

---
+++ utils/linesToHighlight.js
@@ -0,0 +1,25 @@
+export const linesToHighlight = (linesString) => {
+	if (!linesString) {
+		return [];
+	}
+	const linesStringArr = linesString.replace(/[\{\}]/g, "").split(",");
+	const linesArr = [];
+
+	for (let i = 0; i < linesStringArr.length; i++) {
+		const str = linesStringArr[i];
+		if (str.length === 1) {
+			linesArr.push(Number(str) - 1);
+		} else {
+			const linesBoundaries = str
+				.split("-")
+				.map((lineStr) => Number(lineStr));
+			let fromIndex = linesBoundaries[0] - 1;
+			while (fromIndex < linesBoundaries[1]) {
+				linesArr.push(fromIndex);
+				fromIndex++;
+			}
+		}
+	}
+
+	return linesArr;
+};
