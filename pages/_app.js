7c59fd1c23ef0cbda0e270fee0ab2cb489d717fa
lorenzopepe98 <lore.pepe998@gmail.com>
2020-04-28 17:15:01 +0200

    wrapper around entire app

---
+++ pages/_app.js
@@ -0,0 +1,28 @@
+import { MDXProvider } from "@mdx-js/react";
+import { useRouter } from "next/router";
+
+import { Navbar } from "../Components/Navbar";
+import { CodeBlock } from "../Components/Code/CodeBlock";
+import { getSection } from "../utils/paths";
+
+import "../styles/main.scss";
+
+const components = {
+	code: (props) => <CodeBlock {...props} />,
+	// blockquote: (props) => console.log(props),
+};
+
+const App = ({ Component, pageProps }) => {
+	const router = useRouter();
+	const section = getSection(router.pathname);
+	return (
+		<MDXProvider components={components}>
+			<Navbar section={section} />
+			<section className={`section-${section}`}>
+				<Component {...pageProps} />
+			</section>
+		</MDXProvider>
+	);
+};
+
+export default App;
