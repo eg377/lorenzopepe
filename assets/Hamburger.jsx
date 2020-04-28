c7be04bb52236f2209b6bd7975e9eeb8a323cff2
lorenzopepe98 <lore.pepe998@gmail.com>
2020-04-28 21:27:52 +0200

    added desc tags to svgs that did not have it, and fixed description => desc

--- assets/Hamburger.jsx
+++ assets/Hamburger.jsx
@@ -1,7 +1,7 @@
 export const HamburgerClosed = () => (
 	<svg viewBox="0 0 24 24">
 		<title>Menu Closed Icon</title>
-		<description>Click to open menu</description>
+		<desc>Click to open menu</desc>
 		<path d="M0 0h24v24H0V0z" fill="none" />
 		<path d="M4 18h16c.55 0 1-.45 1-1s-.45-1-1-1H4c-.55 0-1 .45-1 1s.45 1 1 1zm0-5h16c.55 0 1-.45 1-1s-.45-1-1-1H4c-.55 0-1 .45-1 1s.45 1 1 1zM3 7c0 .55.45 1 1 1h16c.55 0 1-.45 1-1s-.45-1-1-1H4c-.55 0-1 .45-1 1z" />
 	</svg>
@@ -10,7 +10,7 @@ export const HamburgerClosed = () => (
 export const HamburgerOpen = () => (
 	<svg viewBox="0 0 24 24">
 		<title>Menu Open Icon</title>
-		<description>Click to close menu</description>
+		<desc>Click to close menu</desc>
 		<path d="M0,0h24v24H0V0z" fill="none" />
 		<path d="M4,18h11c0.55,0,1-0.45,1-1v0c0-0.55-0.45-1-1-1H4c-0.55,0-1,0.45-1,1v0C3,17.55,3.45,18,4,18z M4,13h8c0.55,0,1-0.45,1-1v0 c0-0.55-0.45-1-1-1H4c-0.55,0-1,0.45-1,1v0C3,12.55,3.45,13,4,13z M3,7L3,7c0,0.55,0.45,1,1,1h11c0.55,0,1-0.45,1-1v0 c0-0.55-0.45-1-1-1H4C3.45,6,3,6.45,3,7z M20.3,14.88L17.42,12l2.88-2.88c0.39-0.39,0.39-1.02,0-1.41l0,0 c-0.39-0.39-1.02-0.39-1.41,0l-3.59,3.59c-0.39,0.39-0.39,1.02,0,1.41l3.59,3.59c0.39,0.39,1.02,0.39,1.41,0l0,0 C20.68,15.91,20.69,15.27,20.3,14.88z" />
 		<path d="M0,0h24v24H0V0z" fill="none" />
