8f7bd0f2191490d48db92ba2aea195c9e7214e8f
lorenzopepe98 <lore.pepe998@gmail.com>
2020-04-29 01:08:13 +0200

    added meta/seo

--- pages/blog.js
+++ pages/blog.js
@@ -1,22 +1,30 @@
 import { PostPreview } from "../Components/Blog/PostPreview";
 import { extractMetadata } from "../utils/extractMetadata";
 import { timestampToDate } from "../utils/timestampToDate";
+import Head from "next/head";
+import { Fragment } from "react";
 
 const BlogList = ({ postsData }) => {
 	return (
-		<ul className="blog-post-list-preview">
-			{postsData.map((postData) => (
-				<PostPreview
-					key={postData.timestamp}
-					title={postData.title}
-					dateString={timestampToDate(postData.timestamp)}
-					tags={postData.tags}
-					href={postData.href}
-				>
-					{postData.description}
-				</PostPreview>
-			))}
-		</ul>
+		<Fragment>
+			<Head>
+				<title>Blog</title>
+				<meta name="description" content="Lorenzo Pepe Blog" />
+			</Head>
+			<ul className="blog-post-list-preview">
+				{postsData.map((postData) => (
+					<PostPreview
+						key={postData.timestamp}
+						title={postData.title}
+						dateString={timestampToDate(postData.timestamp)}
+						tags={postData.tags}
+						href={postData.href}
+					>
+						{postData.description}
+					</PostPreview>
+				))}
+			</ul>
+		</Fragment>
 	);
 };
 
