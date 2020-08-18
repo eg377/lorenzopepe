8f3d0a72b3ec51c9d432941291a4a412eecdbfb3
lorenzopepe98 <lore.pepe998@gmail.com>
2020-08-18 18:13:28 +0200

    rewrote navbar component/styles

--- Components/Navbar.jsx
+++ Components/Navbar.jsx
@@ -3,41 +3,34 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 import { DarkModeToggle } from "./DarkModeToggle";
 import { HamburgerClosed, HamburgerOpen } from "../assets/Hamburger";
-import { Portfolio } from "../assets/Portfolio";
-import { Blog } from "../assets/Blog";
 import { About } from "../assets/About";
+import { Rocky } from "../assets/Rocky";
 
 export const Navbar = ({ section }) => {
 	const [menuOpen, setMenuOpen] = useState(false);
 	const router = useRouter();
-
 	useEffect(() => {
 		setMenuOpen(false);
 	}, [router.pathname]);
 
 	return (
-		<div className={`nav-wrapper ${menuOpen ? "open" : "closed"}`}>
+		<header className={`nav-wrapper ${menuOpen ? "open" : "closed"}`}>
 			<nav className="navbar" role="navigation">
 				<ul>
-					<li>
-						<Link href="/">
-							<a className={isCurrent(section, "portfolio")}>
-								<Portfolio />
-								<span>Portfolio</span>
-							</a>
-						</Link>
-					</li>
-
-					<li>
+					<li className="logo">
 						<Link href="/blog">
 							<a className={isCurrent(section, "blog")}>
-								<Blog />
-								<span>Blog</span>
+								<Rocky />
+								<span>
+									LORENZO
+									<br />
+									PEPE
+								</span>
 							</a>
 						</Link>
 					</li>
 
-					<li>
+					<li className="nav-about">
 						<Link href="/about">
 							<a className={isCurrent(section, "about")}>
 								<About />
@@ -51,7 +44,19 @@ export const Navbar = ({ section }) => {
 			<button className="menu-btn" onClick={() => setMenuOpen(!menuOpen)}>
 				{menuOpen ? <HamburgerOpen /> : <HamburgerClosed />}
 			</button>
-		</div>
+
+			{menuOpen ? (
+				<div className="menu-bg-svg-wrapper">
+					<svg className="menu-bg-svg" viewBox="0 0 698.97 611.5">
+						<path
+							transform="scale(1.3) translate(-100, -60)"
+							strokeMiterlimit="10"
+							d="M30.7 301.86c-29.19 21.81-50 90.7 0 309.14h647.83c30.2-456.79 22.72-606.36 0-610.42C648.28-4.81 602.24 249.79 522.66 249c-54.85-.53-72.92-121.87-130.54-121.41-65.44.53-72.24 157.26-170.48 194.48-82.8 31.4-144.95-54.57-190.94-20.21z"
+						/>
+					</svg>
+				</div>
+			) : null}
+		</header>
 	);
 };
 
