1e8d2914714c88719a38dd4fa7a406e4df024708
lorenzopepe98 <lore.pepe998@gmail.com>
2020-08-18 22:06:48 +0200

    added redirect from /blog to / after removing blog section

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 const withMDX = require("@next/mdx")({
 	extension: /\.mdx?$/,
 });
+
 module.exports = withMDX({
 	pageExtensions: ["js", "jsx", "md", "mdx"],
+	async redirects() {
+		return [
+			{
+				source: "/blog",
+				destination: "/",
+				permanent: true,
+			},
+		];
+	},
 });
