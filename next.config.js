3a6a0bd48c19afce783664e88784ab7a001380e8
lorenzopepe98 <lore.pepe998@gmail.com>
2020-12-03 02:17:40 +0100

    added rehype shiki

--- next.config.js
+++ next.config.js
@@ -1,9 +1,25 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const rehypeShiki = require("rehype-shiki");
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
 	enabled: process.env.ANALYZE === "true",
 });
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const withMDX = require("@next/mdx")({
 	extension: /\.mdx?$/,
+	options: {
+		rehypePlugins: [
+			[
+				rehypeShiki,
+				{
+					theme: "./utils/material-theme-palenight.json",
+					useBackground: true,
+				},
+			],
+		],
+	},
 });
 
 module.exports = withBundleAnalyzer(
