3b6405ae89118275cdc4e497651ad605138737c0
lorenzopepe98 <lore.pepe998@gmail.com>
2021-05-07 18:35:00 +0200

    using scss modules

--- BlogDemos/IrradiationIllusion/index.tsx
+++ BlogDemos/IrradiationIllusion/index.tsx
@@ -1,15 +1,23 @@
+import style from "./index.module.scss";
+
 export const IrradiationIllusion: React.FC = () => {
 	return (
-		<div className="irradiation-illusion">
+		<div className={style.irradiation_illusion}>
 			<code className="inline-code">font-weight: 300</code>
-			<div className="example-container" style={{ fontWeight: 300 }}>
-				<div className="light">The quick brown fox jumps</div>
-				<div className="dark">The quick brown fox jumps</div>
+			<div
+				className={style.example_container}
+				style={{ fontWeight: 300 }}
+			>
+				<div className={style.light}>The quick brown fox jumps</div>
+				<div className={style.dark}>The quick brown fox jumps</div>
 			</div>
 			<code className="inline-code">font-weight 400</code>
-			<div className="example-container" style={{ fontWeight: 400 }}>
-				<div className="light">The quick brown fox jumps</div>
-				<div className="dark">The quick brown fox jumps</div>
+			<div
+				className={style.example_container}
+				style={{ fontWeight: 400 }}
+			>
+				<div className={style.light}>The quick brown fox jumps</div>
+				<div className={style.dark}>The quick brown fox jumps</div>
 			</div>
 		</div>
 	);
